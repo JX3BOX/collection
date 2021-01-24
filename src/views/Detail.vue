41e45b5a1d800319a0d67d5d2cdc83e3f27f7d1b
Kuguats <445786355@qq.com>
2021-01-24 19:25:02 +0800

    fix

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -173,15 +173,12 @@
             </template>
         </div>
 
-        <div class="m-tags">
+        <div class="m-tags" v-if="collection.tags && collection.tags.length">
             <div class="u-tags-title">
                 <i class="el-icon-price-tag"></i>
                 <span> 标签</span>
             </div>
-            <ul
-                class="u-tags"
-                v-if="collection.tags && collection.tags.length"
-            >
+            <ul class="u-tags">
                 <li
                     v-for="(tag, key) in collection.tags"
                     :key="key"
